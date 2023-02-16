efafab5bfab4e02f7576074409aba8dbc0450c86
Григоренко Александр Игоревич <aigrigorenko@mtsbank.ru>
2023-02-16 15:38:05 +0500

    makeScene

--- CleanSwift/CleanSwift/SpaceImageScreen/View/SpaceImageScreenViewController.swift
+++ CleanSwift/CleanSwift/SpaceImageScreen/View/SpaceImageScreenViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SpaceImageScreenViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
