bc1019621adc0f2664387f8af77ca5a886d7bc11
Григоренко Александр Игоревич <aigrigorenko@mtsbank.ru>
2023-02-16 15:36:56 +0500

    initCommit

---
+++ CleanSwift/CleanSwift/SpaceImageScreen/View/SpaceImageScreenDisplayLogic.swift
@@ -0,0 +1,8 @@
+//
+//  SpaceImageScreenDisplayLogic.swift
+//  CleanSwift
+//
+//  Created by Григоренко Александр Игоревич on 16.02.2023.
+//
+
+import Foundation
---
+++ CleanSwift/CleanSwift/SpaceImageScreen/View/SpaceImageScreenViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  CleanSwift
+//
+//  Created by Григоренко Александр Игоревич on 16.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
